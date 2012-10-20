873a11b69b39e876e61d4149e561abc8d03472f1
Johannes D. Wirde <johannes.wirde@gmail.com>
2012-10-20 14:45:06 +0200

    Done.

--- src/parse.py
+++ src/parse.py
@@ -15,19 +15,6 @@ class SolarSystem(object):
         for p in self.planets:
             ret += "\tName: " + p["name"] + ", property: " + p["prop"] + "\n"
 
-        if self.has_starport:
-            ret += "starport: yes - " + self.starport_property["quality"] + "\n"
-            ret +=  self.starport_property["text"] + "\n"
-        else:
-            ret += "starport: no\n"
-        if self.has_navalbase:
-            ret += "naval base: yes - " + self.navalbase_property["quality"] + "\n"
-            if self.navalbase_property["text"]:
-                ret +=  self.navalbase_property["text"] + "\n"
-        else:
-            ret += "naval base: no\n"
-
-
         return ret
 
 def parse(soup):
@@ -44,15 +31,6 @@ def parse(soup):
             resulting_planet["name"] = tds[1].find("b").text
         elif len(tds) == 1:
             resulting_planet["prop"] = tds[0].text
-
-    for td in soup.find(text="Starport:").parent.parent.findAll("td", {"class": "value"}):
-        system.has_starport = True
-        system.starport_property = {"quality":td.text, "text": td.parent.findNextSibling().text}
-
-    for td in soup.find(text="Naval Base:").parent.parent.findAll("td", {"class": "value"}):
-        system.has_navalbase = True
-        system.navalbase_property = {"quality":td.text, "text": td.parent.findNextSibling().text}
-
 
     return system
 
